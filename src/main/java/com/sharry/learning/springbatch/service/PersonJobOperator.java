35fe8adb75145a7663d136a82f1960192cc110a6
Sharry Shuai <Sharry.Shuai@activenetwork.com>
2018-08-16 15:31:57 +0800

    Disabled the code.

--- src/main/java/com/sharry/learning/springbatch/service/PersonJobOperator.java
+++ src/main/java/com/sharry/learning/springbatch/service/PersonJobOperator.java
@@ -9,65 +9,63 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobInstance;
 import org.springframework.batch.core.configuration.JobRegistry;
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.support.ReferenceJobFactory;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.launch.JobOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import com.sharry.learning.springbatch.PersonBatchApp;
 
-@Configuration
-@EnableBatchProcessing
+//@Configuration
+//@EnableBatchProcessing
 //@Component
 public class PersonJobOperator {
     
-    private static Logger logger = LoggerFactory.getLogger(PersonJobOperator.class);
-    
-    @Autowired
-    private JobExplorer jobExplorer;
-    @Autowired
-    private JobOperator jobOperator;
-    @Autowired
-    private JobRegistry jobRegistry;
-    
-    public List<JobInstance> getJobInstances(String jobName, int start, int count) {
-        return this.jobExplorer.getJobInstances(jobName, start, count);
-    }
-    
-    public List<JobExecution> getJobExecutions(JobInstance jobInstance) {
-        return this.jobExplorer.getJobExecutions(jobInstance);
-    }
-    
-    public Long restart(Job job, Long jobInstanceId) throws Exception {
-         this.jobRegistry.register(new ReferenceJobFactory(job));
-         return this.jobOperator.restart(jobInstanceId);
-    }
-    
-    @SuppressWarnings("resource")
-    public static void main(String[] args) throws Exception {
-        
-        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-batch-person.xml");
-        PersonJobOperator operator = (PersonJobOperator) appContext.getBean("personJobOperator");
-        PersonBatchJobConfig pjc = (PersonBatchJobConfig) appContext.getBean("personBatchJobConfig");
-        Job jobObj = pjc.job(PersonBatchApp.buildPersonDOList(10));
-        
-        List<JobInstance> instances = operator.getJobInstances("jobImportingPersons", 0, 1);
-        JobInstance jobInstance = instances.get(0);
-        logger.debug("JobInstance Info:\r\nid={}\r\ninstanceId={}\r\njob name={}", jobInstance.getId(), jobInstance.getInstanceId(), jobInstance.getJobName());
-        
-        List<JobExecution> executions = operator.getJobExecutions(jobInstance);
-        for (JobExecution jobExecution : executions) {
-            if(jobExecution.getStatus().name().equals("FAILED")) {
-                logger.debug("JobExecution info:\r\nid={}\r\nstatus={}\r\nexit status={}", jobExecution.getId(), jobExecution.getStatus().name(), jobExecution.getExitStatus());
-                operator.restart(jobObj, jobInstance.getId());
-                jobExecution.upgradeStatus(BatchStatus.COMPLETED);
-                break;
-            }
-        }
-        
-    }
+//    private static Logger logger = LoggerFactory.getLogger(PersonJobOperator.class);
+//    
+//    @Autowired
+//    private JobExplorer jobExplorer;
+//    @Autowired
+//    private JobOperator jobOperator;
+//    @Autowired
+//    private JobRegistry jobRegistry;
+//    
+//    public List<JobInstance> getJobInstances(String jobName, int start, int count) {
+//        return this.jobExplorer.getJobInstances(jobName, start, count);
+//    }
+//    
+//    public List<JobExecution> getJobExecutions(JobInstance jobInstance) {
+//        return this.jobExplorer.getJobExecutions(jobInstance);
+//    }
+//    
+//    public Long restart(Job job, Long jobInstanceId) throws Exception {
+//         this.jobRegistry.register(new ReferenceJobFactory(job));
+//         return this.jobOperator.restart(jobInstanceId);
+//    }
+//    
+//    @SuppressWarnings("resource")
+//    public static void main(String[] args) throws Exception {
+//        
+//        ApplicationContext appContext = new ClassPathXmlApplicationContext("spring-batch-person.xml");
+//        PersonJobOperator operator = (PersonJobOperator) appContext.getBean("personJobOperator");
+//        PersonBatchJobConfig pjc = (PersonBatchJobConfig) appContext.getBean("personBatchJobConfig");
+//        Job jobObj = pjc.job(PersonBatchApp.buildPersonDOList(10));
+//        
+//        List<JobInstance> instances = operator.getJobInstances("jobImportingPersons", 0, 1);
+//        JobInstance jobInstance = instances.get(0);
+//        logger.debug("JobInstance Info:\r\nid={}\r\ninstanceId={}\r\njob name={}", jobInstance.getId(), jobInstance.getInstanceId(), jobInstance.getJobName());
+//        
+//        List<JobExecution> executions = operator.getJobExecutions(jobInstance);
+//        for (JobExecution jobExecution : executions) {
+//            if(jobExecution.getStatus().name().equals("FAILED")) {
+//                logger.debug("JobExecution info:\r\nid={}\r\nstatus={}\r\nexit status={}", jobExecution.getId(), jobExecution.getStatus().name(), jobExecution.getExitStatus());
+//                operator.restart(jobObj, jobInstance.getId());
+//                jobExecution.upgradeStatus(BatchStatus.COMPLETED);
+//                break;
+//            }
+//        }
+//        
+//    }
     
 }
